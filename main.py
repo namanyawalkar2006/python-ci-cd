103b4d7fc922d1d50a9073dc5d2b43f1b8034cca
Naman Yawalkar <namanyawalkar2006@gmail.com>
2026-07-08 14:58:35 +0545

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+def add (a,b):
+    return a + b
+
+def substract (a,b):
+    return a - b
+
+if __name__ == "__main__":
+    print("Addition of 5 and 3 is:", add(5, 3))
+    print("Subtraction of 5 and 3 is:", substract(5, 3))
---
+++ test_main.py
@@ -0,0 +1,7 @@
+from main import add, substract
+
+def test_add():
+    assert add(5, 3) == 8
+
+def test_substract():
+    assert substract(5, 3) == 2
